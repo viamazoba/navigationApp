4ca18887c9d58ea3003c38471fb3d256b44246ab
Victor Mazo <viamazoba@unal.edu.co>
2025-01-26 14:55:09 -0500

    build: Configuración de react native navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Text, View } from "react-native"
+import { NavigationContainer } from "@react-navigation/native";
+import { Text } from "react-native"
 
 export const App = () => {
 
   return (
-    <View>
+    <NavigationContainer>
       <Text>App.tsx</Text>
-    </View>
+    </NavigationContainer>
   );
 };
