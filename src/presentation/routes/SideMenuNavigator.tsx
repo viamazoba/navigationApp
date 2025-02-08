26b122dfce53245bb284bcfb5148d8065a95f38c
Victor Mazo <viamazoba@unal.edu.co>
2025-02-08 14:01:07 -0500

    feat: Se realiza primera personalización del drawer

--- src/presentation/routes/SideMenuNavigator.tsx
+++ src/presentation/routes/SideMenuNavigator.tsx
@@ -1,14 +1,50 @@
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, type DrawerContentComponentProps, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import { StackNavigator } from './StackNavigator';
 import { ProfileScreen } from '../screens/profile/ProfileScreen';
+import { globalColors } from '../theme/theme';
+import { useWindowDimensions, View } from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
 export const SideMenuNavigator = () => {
+
+    const dimensions = useWindowDimensions();
     return (
-        <Drawer.Navigator>
+        <Drawer.Navigator
+            drawerContent={(props) => <CustomDrawerContent {...props} />}
+            screenOptions={{
+                headerShown: false,
+                drawerType: (dimensions.width >= 758) ? 'permanent' : 'front',
+                drawerActiveBackgroundColor: globalColors.primary,
+                drawerActiveTintColor: 'white',
+                drawerInactiveTintColor: globalColors.primary,
+                drawerItemStyle: {
+                    borderRadius: 10,
+                    paddingHorizontal: 20
+                }
+            }}
+        >
             <Drawer.Screen name="StackNavigator" component={StackNavigator} />
             <Drawer.Screen name="Profile" component={ProfileScreen} />
         </Drawer.Navigator>
     );
+}
+
+const CustomDrawerContent = (props: DrawerContentComponentProps) => {
+
+    return (
+        <DrawerContentScrollView>
+            <View
+                style={{
+                    height: 200,
+                    backgroundColor: globalColors.primary,
+                    margin: 30,
+                    borderRadius: 50
+                }}
+            />
+
+            <DrawerItemList {...props} />
+
+        </DrawerContentScrollView>
+    )
 }
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,13 +1,24 @@
-import { View } from "react-native"
+import { Pressable, Text, View } from "react-native"
 import { globalStyles } from "../../theme/theme"
-import { type NavigationProp, useNavigation } from "@react-navigation/native"
+import { DrawerActions, type NavigationProp, useNavigation } from "@react-navigation/native"
 import { PrimaryButton } from "../../components/shared/PrimaryButton"
 import { type RootStackParams } from "../../routes/StackNavigator"
+import { useEffect } from "react"
 
 
 export const HomeScreen = () => {
 
     const navigation = useNavigation<NavigationProp<RootStackParams>>()
+
+    useEffect(() => {
+        navigation.setOptions({
+            headerLeft: () => (
+                <Pressable onPress={() => navigation.dispatch(DrawerActions.toggleDrawer)}>
+                    <Text>Menu</Text>
+                </Pressable>
+            )
+        })
+    }, [])
 
     return (
         <View style={globalStyles.container}>
