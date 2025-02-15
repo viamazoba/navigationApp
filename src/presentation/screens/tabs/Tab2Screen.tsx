90a4623a6085f2b3e4475e3da7b448c6624280aa
Victor Mazo <viamazoba@unal.edu.co>
2025-02-15 12:05:51 -0500

    feat: Se implementa el menu hamburguesa en las diferentes bottom tabs

--- src/presentation/screens/tabs/Tab2Screen.tsx
+++ src/presentation/screens/tabs/Tab2Screen.tsx
@@ -1,10 +1,12 @@
 import { Text, View } from "react-native"
+import { HamburgerMenu } from "../../components/shared/HamburgerMenu"
 
 
 export const Tab2Screen = () => {
 
     return (
         <View>
+            <HamburgerMenu />
             <Text>Tab2Screen</Text>
         </View>
     )
--- src/presentation/screens/tabs/Tab3Screen.tsx
+++ src/presentation/screens/tabs/Tab3Screen.tsx
@@ -1,10 +1,12 @@
 import { Text, View } from "react-native"
+import { HamburgerMenu } from "../../components/shared/HamburgerMenu"
 
 
 export const Tab3Screen = () => {
 
     return (
         <View>
+            <HamburgerMenu />
             <Text>Tab3Screen</Text>
         </View>
     )
